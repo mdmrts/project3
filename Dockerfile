188bd581a121b2074fe8a548ccb5e99656412129
kaw393939 <keith@webizly.com>
2021-11-29 20:29:38 -0500

    Updated for Flask with form examples

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,13 @@
 FROM python:3.8-buster
+ENV FLASK_APP=app/app.py
+ENV FLASK_RUN_HOST=0.0.0.0
+ENV FLASK_ENV=development
 RUN apt-get update
+RUN /usr/local/bin/python -m pip install --upgrade pip
 RUN adduser myuser
 USER myuser
 WORKDIR /home/myuser
 ENV PATH="/home/myuser/.local/bin:${PATH}"
 COPY --chown=myuser:myuser . .
 RUN pip install -r requirements.txt
+CMD ["uwsgi", "app/app.ini"]
--- calc/calculator.py
+++ calc/calculator.py
@@ -12,17 +12,17 @@ class Calculator:
         return Calculations.get_last_calculation_result_value()
     @staticmethod
     #tuple allows me to pass in as many values as a I want
-    def add_numbers(tuple_values: tuple):
+    def addition(tuple_values: tuple):
         """ adds list of numbers"""
         Calculations.add_addition_calculation_to_history(tuple_values)
         return True
     @staticmethod
-    def subtract_numbers(tuple_values: tuple):
+    def subtraction(tuple_values: tuple):
         """ subtract a list of numbers from result"""
         Calculations.add_subtraction_calculation_to_history(tuple_values)
         return True
     @staticmethod
-    def multiply_numbers(tuple_values: tuple):
+    def multiplication(tuple_values: tuple):
         """ multiplication number from result"""
         Calculations.add_multiplication_calculation_to_history(tuple_values)
         return True
